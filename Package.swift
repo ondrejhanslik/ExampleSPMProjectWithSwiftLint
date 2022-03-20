fb094339b0274beb9b3ee39fed99e59dc0a7a158
Ondřej Hanslík <ondrej.hanslik@gmail.com>
2022-03-20 23:39:38 +0100

    Plugin product

--- Package.swift
+++ Package.swift
@@ -5,25 +5,21 @@ import PackageDescription
 let package = Package(
     name: "MyApp",
     products: [
-        .executable(name: "MyApp", targets: ["MyApp"]),
+        .plugin(
+            name: "SwiftLint",
+            targets: ["SwiftLintXcode"]
+        ),
     ],
     targets: [
-        // 1. Specify where to download the compiled swiftlint tool from.
         .binaryTarget(
             name: "SwiftLintBinary",
             url: "https://github.com/juozasvalancius/SwiftLint/releases/download/spm-accommodation/SwiftLintBinary-macos.artifactbundle.zip",
             checksum: "cdc36c26225fba80efc3ac2e67c2e3c3f54937145869ea5dbcaa234e57fc3724"
         ),
-        // 2. Define the SPM plugin.
         .plugin(
             name: "SwiftLintXcode",
             capability: .buildTool(),
             dependencies: ["SwiftLintBinary"]
-        ),
-        // 3. Use the plugin in your project.
-        .executableTarget(
-            name: "MyApp",
-            plugins: ["SwiftLintXcode"]
         )
     ]
 )
